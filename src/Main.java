028dbd8c48f43092a36253cb068122eec3faaec5
Spencer Jacobs <sjacobs@zagmail.gonzaga.edu>
2019-10-29 14:05:46 -0700

    added comment

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,9 @@ public class Main {
         System.out.println("Hello grandma");
     }
 }
+
+
+//what is a branch
+//  an alternative to 'master' where you can safely develop new features, fix bugs, try new things
+//  we can try our a new feature and then when it passes a bunch of tests then we can
+//  commit it to the master branch which will later be released to the user
